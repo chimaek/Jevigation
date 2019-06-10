b344b3ffb188aec86ae2a3888c5a148f88b7169a
chimaek <pipiru100@gmail.com>
2019-06-10 21:58:45 +0900

    수정

--- tensorflow/examples/android/src/org/tensorflow/demo/MainActivity.java
+++ tensorflow/examples/android/src/org/tensorflow/demo/MainActivity.java
@@ -21,7 +21,7 @@ public class MainActivity extends Activity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(getApplicationContext(), DetectorActivity.class));
+                startActivity(new Intent(getApplicationContext(),DetectorActivity.class));
             }
         });
     }
